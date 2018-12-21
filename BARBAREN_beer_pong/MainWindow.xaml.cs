5d7c3e30de6e10b93471441be725874f827aae5c
LarsNyborgPedersen <larsnyborgpedersen@gmail.com>
2018-12-21 15:46:27 +0100

    Add files via upload

---
+++ BARBAREN_beer_pong/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using BARBAREN_beer_pong.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BARBAREN_beer_pong
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new BlueViewModel();
+        }
+
+        private void blueButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            DataContext = new BlueViewModel();
+        }
+
+        private void redButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            DataContext = new RedViewModel();
+        }
+    }
+}
